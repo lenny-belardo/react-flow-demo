5b9555d47392bb39b4b603c7ca0d9f8a44715694
x-factor <raoul@appfactor.io>
2025-05-04 17:11:33 +0300

    mock-graphql-apis: implemented the response handler

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,7 +1,21 @@
-import { graphql } from 'msw';
+import { graphql, HttpResponse } from 'msw';
+
+const applicationGraph = new Map([
+    [
+        'e82f332c-a4e7-4463-b440-59bc91792634',
+        {
+          id: 'e82f332c-a4e7-4463-b440-59bc91792634',
+          title: 'Introducing a new JavaScript runtime'
+        }
+    ]
+]);
 
 export const handlers = [
-    graphql.query('ApplicationGraph', ({ query }) => {
-        console.log('Intercepted a "ApplicationGraph" GraphQL query:', query);
+    graphql.query('ApplicationGraph', () => {
+        return HttpResponse.json({
+            data: {
+                applicationGraph: Array.from(applicationGraph.values())
+            }
+        });
     })
 ];
