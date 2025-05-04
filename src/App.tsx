7bd82aed3154f749c1f0df07e3500e160f581dd5
x-factor <raoul@appfactor.io>
2025-05-04 21:21:02 +0300

    add-the-apollo-client-and-use-the-applicationgraph-api-in-the-ui: got the applicationGraph data from the graphql query

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,17 @@
+import { useQuery, gql } from '@apollo/client';
+
+const GET_APPLICATION_GRAPH = gql`
+  query ApplicationGraph {
+    applicationGraph {
+      id
+    }
+  }
+`;
+
 function App() {
+  const { data } = useQuery(GET_APPLICATION_GRAPH);
+  console.log("applicationGraph data ", data.applicationGraph);
+
   return (
     <>
       Brand new
