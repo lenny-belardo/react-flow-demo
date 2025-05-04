0e498aa663d762433be0373184c213afc4df4ea7
x-factor <raoul@appfactor.io>
2025-05-04 22:13:26 +0300

    add-react-flow-to-the-project-and-create-a-poc: added a react-flow POC

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import { useQuery, gql } from '@apollo/client';
+import { ReactFlow } from '@xyflow/react';
+
+import '@xyflow/react/dist/style.css';
 
 const GET_APPLICATION_GRAPH = gql`
   query ApplicationGraph {
@@ -10,12 +13,19 @@ const GET_APPLICATION_GRAPH = gql`
 
 function App() {
   const { data } = useQuery(GET_APPLICATION_GRAPH);
-  console.log("applicationGraph data ", data.applicationGraph);
+  console.log("applicationGraph data ", data?.applicationGraph);
+
+  const initialNodes = [
+    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+    { id: '2', position: { x: 0, y: 100}, data: { label: '2' } }
+  ];
+
+  const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
   return (
-    <>
-      Brand new
-    </>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    </div>
   )
 }
 
